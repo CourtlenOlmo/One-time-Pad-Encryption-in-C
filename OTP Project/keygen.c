c6cfe827e85ab1af1a0a505e32b2f9703b548ea8
Courtlen Olmo <46169175+CourtlenOlmo@users.noreply.github.com>
2025-03-09 21:04:32 +0000

    test

---
+++ OTP Project/keygen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+    if (argc != 2) {
+        fprintf(stderr, "You must enter a key length\n");
+        return 1;
+    }
+
+    int keyLength = atoi(argv[1]);
+    char key[keyLength];
+    char keyChars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+    for (int i = 0; i < keyLength; i++){
+        key[i] = keyChars[rand() % 27];
+    }
+    
+    fprintf(stdout, "%s\n", key);
+}
