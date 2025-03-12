eaf54e360e1ea80c4f39a48bcea93eb5b73465db
Courtlen Olmo <46169175+CourtlenOlmo@users.noreply.github.com>
2025-03-12 17:01:23 +0000

    Inital setup of decryption server and client

--- dec_client.c
+++ dec_client.c
@@ -1,3 +1,171 @@
-int main(){
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>  // ssize_t
+#include <sys/socket.h> // send(),recv()
+#include <netdb.h>      // gethostbyname()
+
+/**
+* Client code
+* 1. Create a socket and connect to the server specified in the command arugments.
+* 2. Prompt the user for input and send that input as a message to the server.
+* 3. Print the message received from the server and exit the program.
+*/
+
+// Error function used for reporting issues
+void error(const char *msg) { 
+  perror(msg); 
+  exit(0); 
+} 
+
+// Set up the address struct
+void setupAddressStruct(struct sockaddr_in* address, 
+                        int portNumber, 
+                        char* hostname){
+ 
+  // Clear out the address struct
+  memset((char*) address, '\0', sizeof(*address)); 
+
+  // The address should be network capable
+  address->sin_family = AF_INET;
+  // Store the port number
+  address->sin_port = htons(portNumber);
+
+  // Get the DNS entry for this host name
+  struct hostent* hostInfo = gethostbyname(hostname); 
+  if (hostInfo == NULL) { 
+    fprintf(stderr, "CLIENT: ERROR, no such host\n"); 
+    exit(0); 
+  }
+  // Copy the first IP address from the DNS entry to sin_addr.s_addr
+  memcpy((char*) &address->sin_addr.s_addr, 
+        hostInfo->h_addr_list[0],
+        hostInfo->h_length);
+}
+
+void comp_length( char* key, char* fileName){
+    //compare the key against fileName, if it is shorter, print to stderr and exit program
+    FILE *keyFile = fopen(key, "r");
+    FILE *file = fopen(fileName, "r");
+    fseek(keyFile, 0, SEEK_END);
+    long keySize = ftell(keyFile);
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    if (keySize < fileSize){
+      fprintf(stderr, "Error: key '%s' is too short\n", key);
+      exit(1);
+    }
+    fclose(keyFile);
+    fclose(file);
+}
+
+int main(int argc, char *argv[]) {
+  int socketFD, charsWritten, charsRead;
+  struct sockaddr_in serverAddress;
+  char* fileName = argv[1];
+  char* key = argv[2];
+  // Check usage & args
+  if (argc < 3) { 
+    fprintf(stderr,"USAGE: %s hostname port\n", argv[0]); 
+    exit(0); 
+  } 
+
+  comp_length(key, fileName);
+
+  // Create a socket
+  socketFD = socket(AF_INET, SOCK_STREAM, 0); 
+  if (socketFD < 0){
+    error("CLIENT: ERROR opening socket");
+  }
+
+   // Set up the server address struct
+  setupAddressStruct(&serverAddress, atoi(argv[3]), "localhost");
+
+  // Connect to server
+  if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0){
+    error("CLIENT: ERROR connecting");
+  }
+
+  // Open the key and filename files
+  FILE* keyFile = fopen(key, "r");
+  FILE* file = fopen(fileName, "r");
+
+  //put how long keyFile is into a variable
+  fseek(keyFile, 0, SEEK_END);
+  long keySize = ftell(keyFile);
+  fseek(keyFile, 0, SEEK_SET);
+  //put how long file is into a variable
+  fseek(file, 0, SEEK_END);
+  long fileSize = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  //initalize buffer to be the size of the key and file
+  char buffer[keySize + fileSize + 2];
+  memset(buffer, '\0', sizeof(buffer));
+
+  // Read the contents of the file into a temporary buffer
+  char tempBuffer[fileSize + 1];
+  fread(tempBuffer, sizeof(char), fileSize, file);
+  tempBuffer[fileSize] = '\0';
+
+  // Copy the contents of the file into the buffer, removing newline characters
+  int i = 0;
+  for (int j = 0; j < fileSize; j++) {
+    if (tempBuffer[j] != '\n') {
+      buffer[i++] = tempBuffer[j];
+    }
+  }
+  buffer[i++] = '|';
+
+  // Read the contents of the key file into a temporary buffer
+  char tempKeyBuffer[keySize + 1];
+  fread(tempKeyBuffer, sizeof(char), keySize, keyFile);
+  tempKeyBuffer[keySize] = '\0';
+
+  // Copy the contents of the keyfile into the buffer, removing newline characters
+  for (int j = 0; j < keySize; j++) {
+    if (tempKeyBuffer[j] != '\n') {
+      buffer[i++] = tempKeyBuffer[j];
+    }
+  }
+  buffer[i] = '\0';
+
+  
+  // Send message to server while ensuring all data is sent
+  int totalCharsWritten = 0;
+  while (totalCharsWritten < strlen(buffer)){
+    charsWritten = send(socketFD, buffer, strlen(buffer), 0);
+    if (charsWritten < 0){
+      error("CLIENT: ERROR writing to socket");
+    }
+    totalCharsWritten += charsWritten;
+
+  }
+
+  /*
+  charsWritten = send(socketFD, buffer, strlen(buffer), 0); 
+  if (charsWritten < 0){
+    error("CLIENT: ERROR writing to socket");
+  }
+  if (charsWritten < strlen(buffer)){
+    printf("CLIENT: WARNING: Not all data written to socket!\n");
+  }
+  */
+  // Get return message from server
+
+  // Clear out the buffer again for reuse
+  memset(buffer, '\0', sizeof(buffer));
+
+  // Read data from the socket, leaving \0 at end
+  charsRead = recv(socketFD, buffer, sizeof(buffer) - 1, 0); 
+  if (charsRead < 0){
+    error("CLIENT: ERROR reading from socket");
+  }
+  //output buffer to stdout
+  printf("%s\n", buffer);
+
+  // Close the socket
+  close(socketFD); 
+  return 0;
 }
--- dec_server.c
+++ dec_server.c
@@ -1,3 +1,146 @@
-int main(){
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+// Error function used for reporting issues
+void error(const char *msg) {
+  perror(msg);
+  exit(1);
+} 
+
+// Set up the address struct for the server socket
+void setupAddressStruct(struct sockaddr_in* address, 
+                        int portNumber){
+ 
+  // Clear out the address struct
+  memset((char*) address, '\0', sizeof(*address)); 
+
+  // The address should be network capable
+  address->sin_family = AF_INET;
+  // Store the port number
+  address->sin_port = htons(portNumber);
+  // Allow a client at any address to connect to this server
+  address->sin_addr.s_addr = INADDR_ANY;
+}
+
+int main(int argc, char *argv[]){
+  int connectionSocket, charsRead;
+  char mainBuffer[80000];
+  char fileBuffer[80000];
+  char keyBuffer[80000];
+  struct sockaddr_in serverAddress, clientAddress;
+  socklen_t sizeOfClientInfo = sizeof(clientAddress);
+  char* enc_message;
+
+  // Check usage & args
+  if (argc < 2) { 
+    fprintf(stderr,"USAGE: %s port\n", argv[0]); 
+    exit(1);
+  } 
+  
+  // Create the socket that will listen for connections
+  int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if (listenSocket < 0) {
+    error("ERROR opening socket");
+  }
+
+  // Set up the address struct for the server socket
+  setupAddressStruct(&serverAddress, atoi(argv[1]));
+
+  // Associate the socket to the port
+  if (bind(listenSocket, 
+          (struct sockaddr *)&serverAddress, 
+          sizeof(serverAddress)) < 0){
+    error("ERROR on binding");
+  }
+
+  // Start listening for connetions. Allow up to 5 connections to queue up
+  listen(listenSocket, 5); 
+  
+  // Accept a connection, blocking if one is not available until one connects
+  while(1){
+    // Accept the connection request which creates a connection socket
+    connectionSocket = accept(listenSocket, 
+                (struct sockaddr *)&clientAddress, 
+                &sizeOfClientInfo); 
+    if (connectionSocket < 0){
+      error("ERROR on accept");
+    }
+
+    printf("SERVER: Connected to client running at host %d port %d\n", 
+                          ntohs(clientAddress.sin_addr.s_addr),
+                          ntohs(clientAddress.sin_port));
+
+    // Get the message from the client and display it
+    memset(mainBuffer, '\0', 80000);
+    // Receive the plaintext message from the socket
+    charsRead = recv(connectionSocket, mainBuffer, 80000, 0); 
+    if (charsRead < 0){
+      error("ERROR reading from socket");
+    }
+    
+    //seperate the key and file from the buffer and put them into their own buffers
+    char* token = strtok(mainBuffer, "|");
+    strcpy(fileBuffer, token);
+    token = strtok(NULL, "|");
+    strcpy(keyBuffer, token);
+
+    // Assign a numerical value to the letters of the alphabet
+    char* alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+    int alpha[27];
+    for (int i = 0; i < 27; i++){
+      alpha[i] = i;
+    }
+
+    // Initialize enc_message to be the same size as the plaintext message
+    enc_message = malloc((strlen(fileBuffer) + 1) * sizeof(char));
+    memset(enc_message, '\0', strlen(fileBuffer) + 1);
+
+      printf("key: %s\n", keyBuffer);
+      printf("file: %s\n", fileBuffer);
+
+    //iterate through the key and file, adding the value of each letter to the enc_message
+    int enc_message_index = 0;
+    while (1){
+      if (fileBuffer[enc_message_index] == '\0'){
+        break;
+      }
+      char keyChar = keyBuffer[enc_message_index];
+      char fileChar = fileBuffer[enc_message_index];
+      int keyIndex = 0;
+      int fileIndex = 0;
+      for (int i = 0; i < 27; i++){
+        if (keyChar == alphabet[i]){
+          keyIndex = alpha[i];
+        }
+        if (fileChar == alphabet[i]){
+          fileIndex = alpha[i];
+        }
+      }
+      int enc_index = (keyIndex + fileIndex) % 27;
+      enc_message[enc_message_index++] = alphabet[enc_index];
+    }
+
+    enc_message[enc_message_index] = '\0';
+    
+    // Send the encoded message back to the client
+    charsRead = send(connectionSocket, 
+                    enc_message, strlen(enc_message), 0); 
+    if (charsRead < 0){
+      error("ERROR writing to socket");
+    }
+
+    free(enc_message);
+
+    // Close the connection socket for this client
+    close(connectionSocket); 
+  }
+
+  // Close the listening socket
+  close(listenSocket); 
+  return 0;
 }
